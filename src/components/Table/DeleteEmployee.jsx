0685c20bb052361f9c116a42041739087d516d42
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2025-05-10 10:10:23 -0500

    modal eliminado

--- src/components/Table/DeleteEmployee.jsx
+++ src/components/Table/DeleteEmployee.jsx
@@ -1,24 +1,59 @@
 import DeleteIcon from "@mui/icons-material/Delete";
-import { IconButton } from "@mui/material";
+import { IconButton, Button } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
 
 import useDeleteEmployee from "@/hooks/useDeleteEmployee";
 
 const DeleteEmployee = ({ employeeId }) => {
   const { mutate: deleteEmployee, isLoading } = useDeleteEmployee();
+  const [open, setOpen] = useState(false);
 
   const handleOnDelete = () => {
-    deleteEmployee(employeeId);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   return (
-    <IconButton
-      aria-label="delete"
-      color="error"
-      onClick={handleOnDelete}
-      disabled={isLoading}
-    >
-      <DeleteIcon />
-    </IconButton>
+    <>
+      <IconButton
+        aria-label="delete"
+        color="error"
+        onClick={handleOnDelete}
+        disabled={isLoading}
+      >
+        <DeleteIcon />
+      </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Deseas eliminar este empleado?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Esta acción no se puede deshacer. ¿Estás seguro de que deseas
+            eliminar este empleado?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={() => deleteEmployee(employeeId)} autoFocus>
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
